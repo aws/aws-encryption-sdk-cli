c33654af7d8d04efd3665110b0d02b1991e9785f
mattsb42-aws <bullocm@amazon.com>
2017-10-13 20:34:31 -0700

    adding Base64IO context manager stream transformer

---
+++ src/aws_encryption_sdk_cli/internal/encoding.py
@@ -0,0 +1,178 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""Base64 context manager."""
+from __future__ import division
+import base64
+import io
+import logging
+from typing import IO, List, Optional  # noqa pylint: disable=unused-import
+from types import TracebackType  # noqa pylint: disable=unused-import
+
+from wrapt import ObjectProxy
+
+from aws_encryption_sdk_cli.internal.logging_utils import LOGGER_NAME
+
+_LOGGER = logging.getLogger(LOGGER_NAME)
+__all__ = ('Base64IO',)
+
+
+class Base64IO(ObjectProxy):
+    """Wraps a stream, base64-decoding read results before returning them.
+
+    :param wrapped: Stream to wrap
+    """
+
+    # Prime ObjectProxy's attributes to allow setting in init.
+    __read_buffer = None
+    __write_buffer = None
+    __finalize = False
+    __in_context_manager = False
+    closed = False
+
+    def __init__(self, wrapped):
+        # type: (IO) -> None
+        """Check for required methods on wrapped stream and set up read buffer."""
+        required_attrs = ('read', 'write', 'close', 'closed')
+        if not all(hasattr(wrapped, attr) for attr in required_attrs):
+            raise TypeError('Base64IO wrapped object must have attributes: {}'.format(repr(sorted(required_attrs))))
+        super(Base64IO, self).__init__(wrapped)
+        self.__read_buffer = b''
+        self.__write_buffer = b''
+
+    def __enter__(self):
+        # type: () -> Base64IO
+        """Return self on enter."""
+        self.__in_context_manager = True
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):  # ObjectProxy exit confuses pylint: disable=arguments-differ
+        # type: (type, BaseException, TracebackType) -> None
+        """Properly close self on exit."""
+        self.close()
+        self.__in_context_manager = False
+
+    def close(self):
+        # type: () -> None
+        """Closes this stream, encoding and writing any buffered bytes is present.
+
+        .. note::
+
+            This does **not** close the wrapped stream.
+        """
+        self.__finalize = True
+        if self.__write_buffer:
+            self.write(b'')
+        self.closed = True
+
+    def write(self, b):
+        # type: (bytes) -> None
+        """Base64-encode the bytes and write them to the wrapped stream, buffering any
+        bytes that would require padding for the next write call.
+
+        .. warning::
+
+            Because up to two bytes of data must be buffered to ensure correct base64 encoding
+            of all data written, this method is disabled except when the Base64IO object is
+            used as a context manager. This is enforced in order to ensure that your data
+            is not corrupted.
+
+        :param bytes b: Bytes to write to wrapped stream
+        :raises ValueError: if called on closed Base64IO object
+        :raises ValueError: if called on Base64IO object outside of a context manager
+        """
+        if not self.__in_context_manager:
+            raise ValueError('Writes are only allowed on Base64IO objects when used as context managers.')
+
+        if self.closed:
+            raise ValueError('I/O operation on closed file.')
+
+        # Load any stashed bytes and clear the buffer
+        _b = self.__write_buffer + b
+        self.__write_buffer = b''
+
+        # If an even base64 chunk or finalizing the stream, write through.
+        if len(_b) % 3 == 0 or self.__finalize:
+            return self.__wrapped__.write(base64.b64encode(_b))
+
+        # We're not finalizing the stream, so stash the trailing bytes and encode the rest.
+        trailing_byte_pos = -1 * (len(_b) % 3)
+        self.__write_buffer = _b[trailing_byte_pos:]
+        return self.__wrapped__.write(base64.b64encode(_b[:trailing_byte_pos]))
+
+    def read(self, b=None):
+        # type: (Optional[int]) -> bytes
+        """Read bytes from source stream base64-decoding before return, and adjusting read
+        from wrapped stream to return correct number of bytes.
+
+        :param int b: Number of bytes to read
+        :returns: Decoded bytes from wrapped stream
+        :rtype: bytes
+        """
+        if self.closed:
+            raise ValueError('I/O operation on closed file.')
+        _b = None
+        if b is not None:
+            # Calculate number of encoded bytes that must be read to get b raw bytes.
+            _b = int((b - len(self.__read_buffer)) * 4 / 3)
+            _b += (4 - _b % 4)
+
+        _LOGGER.debug('%s bytes requested: adjusted to %s bytes', b, _b)
+
+        # Read encoded bytes from wrapped stream.
+        data = self.__wrapped__.read(_b)
+        _LOGGER.debug('read %d bytes from source', len(data))
+
+        results = io.BytesIO()
+        _LOGGER.debug('loading %d stashed bytes', len(self.__read_buffer))
+        # First, load any stashed bytes
+        results.write(self.__read_buffer)
+        # Decode encoded bytes.
+        results.write(base64.b64decode(data))
+
+        results.seek(0)
+        output_data = results.read(b)
+        _LOGGER.debug('returning %d bytes', len(output_data))
+        # Stash any extra bytes for the next run.
+        self.__read_buffer = results.read()
+        _LOGGER.debug('stashing %d bytes', len(self.__read_buffer))
+
+        if not output_data:
+            self.__wrapped__.close()
+        return output_data
+
+    def __iter__(self):
+        # type: () -> Base64IO
+        """Iterate with this class, not the wrapped stream."""
+        return self
+
+    def readline(self):
+        # type: () -> bytes
+        """Readline with this class, not the wrapped stream."""
+        return self.read(io.DEFAULT_BUFFER_SIZE)
+
+    def readlines(self):
+        # type: () -> List[bytes]
+        """Readlines with this class, not the wrapped stream."""
+        return [line for line in self]
+
+    def __next__(self):
+        # type: () -> bytes
+        """Iterate with this class, not the wrapped stream (Python 3 hook)."""
+        if self.__wrapped__.closed and not self.__read_buffer:
+            raise StopIteration()
+        return self.readline()
+
+    def next(self):
+        # type: () -> bytes
+        """Iterate with this class, not the wrapped stream (Python 2 hook)."""
+        return self.__next__()
---
+++ test/unit/test_encoding.py
@@ -0,0 +1,150 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+"""Unit test suite for ``aws_encryption_sdk_cli.internal.encoding``."""
+import base64
+import io
+import os
+
+import pytest
+
+from aws_encryption_sdk_cli.internal.encoding import Base64IO
+
+
+def test_base64io_bad_wrap():
+    with pytest.raises(TypeError) as excinfo:
+        Base64IO(7)
+
+    excinfo.match(r'Base64IO wrapped object must have attributes: *')
+
+
+def test_base64io_write_outside_context_manager():
+    test = Base64IO(io.BytesIO())
+
+    with pytest.raises(ValueError) as excinfo:
+        test.write(b'asuhdfiouhsad')
+
+    excinfo.match(r'Writes are only allowed on Base64IO objects when used as context managers.')
+
+
+def test_base64io_write_after_closed():
+    with Base64IO(io.BytesIO()) as test:
+        with pytest.raises(ValueError) as excinfo:
+            test.close()
+            test.write(b'aksdhjf')
+
+    excinfo.match(r'I/O operation on closed file.')
+
+
+def test_base64io_read_after_closed():
+    with Base64IO(io.BytesIO()) as test:
+        with pytest.raises(ValueError) as excinfo:
+            test.close()
+            test.read()
+
+    excinfo.match(r'I/O operation on closed file.')
+
+
+TEST_CASES = (
+    (1024, 1024),
+    (222, 222),
+    (1024, None),
+    (1024, 5),
+    (5, 1024)
+)
+
+
+@pytest.mark.parametrize('source_bytes, read_bytes', TEST_CASES)
+def test_base64io_decode(source_bytes, read_bytes):
+    plaintext_source = os.urandom(source_bytes)
+    plaintext_b64 = io.BytesIO(base64.b64encode(plaintext_source))
+    plaintext_wrapped = Base64IO(plaintext_b64)
+
+    test = plaintext_wrapped.read(read_bytes)
+
+    if source_bytes == read_bytes or read_bytes is None:
+        assert test == plaintext_source
+    else:
+        assert test == plaintext_source[:read_bytes]
+
+
+@pytest.mark.parametrize('source_bytes', [case[0] for case in TEST_CASES])
+def test_base64io_encode(source_bytes):
+    plaintext_source = os.urandom(source_bytes)
+    plaintext_b64 = base64.b64encode(plaintext_source)
+    plaintext_stream = io.BytesIO()
+
+    with Base64IO(plaintext_stream) as plaintext_wrapped:
+        plaintext_wrapped.write(plaintext_source)
+
+    assert plaintext_stream.getvalue() == plaintext_b64
+
+
+def test_base64io_decode_context_manager():
+    source_plaintext = os.urandom(102400)
+    source_stream = io.BytesIO(base64.b64encode(source_plaintext))
+
+    test = io.BytesIO()
+    with Base64IO(source_stream) as stream:
+        for chunk in stream:
+            test.write(chunk)
+
+    assert test.getvalue() == source_plaintext
+
+
+def test_base64io_decode_readlines():
+    source_plaintext = os.urandom(102400)
+    source_stream = io.BytesIO(base64.b64encode(source_plaintext))
+
+    test = io.BytesIO()
+    with Base64IO(source_stream) as stream:
+        for chunk in stream.readlines():
+            test.write(chunk)
+
+    assert test.getvalue() == source_plaintext
+
+
+def test_base64io_decode_file(tmpdir):
+    source_plaintext = os.urandom(1024 * 1024)
+    b64_plaintext = tmpdir.join('base64_plaintext')
+    b64_plaintext.write(base64.b64encode(source_plaintext))
+    decoded_plaintext = tmpdir.join('decoded_plaintext')
+
+    with open(str(b64_plaintext), 'rb') as source, open(str(decoded_plaintext), 'wb') as raw:
+        with Base64IO(source) as decoder:
+            for chunk in decoder:
+                raw.write(chunk)
+
+    with open(str(decoded_plaintext), 'rb') as raw:
+        decoded = raw.read()
+
+    assert decoded == source_plaintext
+
+
+def test_base64io_encode_file(tmpdir):
+    source_plaintext = os.urandom(1024 * 1024)
+    plaintext_b64 = base64.b64encode(source_plaintext)
+    plaintext = tmpdir.join('plaintext')
+    b64_plaintext = tmpdir.join('base64_plaintext')
+
+    with open(str(plaintext), 'wb') as file:
+        file.write(source_plaintext)
+
+    with open(str(plaintext), 'rb') as source, open(str(b64_plaintext), 'wb') as target:
+        with Base64IO(target) as encoder:
+            for chunk in source:
+                encoder.write(chunk)
+
+    with open(str(b64_plaintext), 'rb') as file2:
+        encoded = file2.read()
+
+    assert encoded == plaintext_b64
