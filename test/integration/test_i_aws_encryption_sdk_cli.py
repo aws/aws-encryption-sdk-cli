4eacb428f550c695aa4da33ae32b5b0b0980193d
mattsb42-aws <bullocm@amazon.com>
2017-10-20 16:58:53 -0700

    expanding tox linters-tests runs to cover integration tests

--- test/integration/test_i_aws_encryption_sdk_cli.py
+++ test/integration/test_i_aws_encryption_sdk_cli.py
@@ -11,7 +11,7 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 """Integration testing suite for AWS Encryption SDK CLI."""
-from distutils.spawn import find_executable
+from distutils.spawn import find_executable  # distutils confuses pylint: disable=import-error,no-name-in-module
 import filecmp
 import os
 import shlex
