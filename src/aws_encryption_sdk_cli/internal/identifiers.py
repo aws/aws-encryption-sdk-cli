cff3cfdfe1c6b27675b5246c26e90b588dea60f7
mattsb42-aws <bullocm@amazon.com>
2017-10-30 22:51:37 -0700

    adding static plugin namespace divider identifier

--- src/aws_encryption_sdk_cli/internal/identifiers.py
+++ src/aws_encryption_sdk_cli/internal/identifiers.py
@@ -27,3 +27,4 @@ ALGORITHM_NAMES = set([
     alg for alg in dir(aws_encryption_sdk.Algorithm) if not alg.startswith('_')
 ])  # type: Set[aws_encryption_sdk.Algorithm]
 MASTER_KEY_PROVIDERS_ENTRY_POINT = 'aws_encryption_sdk_cli.master_key_providers'
+PLUGIN_NAMESPACE_DIVIDER = '::'
